3c70760554a21e48c977257fe66fb8e729b91dea
丁明杰 <dingmingjie@babytree-inc.com>
2015-06-09 12:11:35 +0800

    create project

---
+++ PrintViewSample/PrintViewSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PrintViewSample
+//
+//  Created by Geass on 6/9/15.
+//  Copyright (c) 2015 BabyTree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PrintViewSample/PrintViewSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PrintViewSample
+//
+//  Created by Geass on 6/9/15.
+//  Copyright (c) 2015 BabyTree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
